6eb29f5408438e931efb7801227f6f5d658a2105
yanglin <lin.yang.neu@neusoft.com>
2023-12-21 15:21:23 +0800

    feat: 🚀 添加备案号、tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.byl-319.top',
-	integrations: [mdx(), sitemap()],
+  site: 'https://www.byl-319.top',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
